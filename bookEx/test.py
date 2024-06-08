1fd1789cfd3b82057a7a81008ad5e48a64aead47
ArtGz <agonza384@calstatela.edu>
2024-06-07 22:20:26 -0700

    Create test.py

---
+++ bookEx/test.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from bookMng.models import Book
+
+
+class BookTestCase(TestCase):
+    def setUp(self):
+        Book.objects.create(name='Django', web='https://www.google.com', price=5.99)
+        Book.objects.create(name='Web', web='https://www.google.com', price=5.99, average=4.3)
+
+    def test_animals_can_speak(self):
+        """Animals that can speak are correctly identified"""
+        djan = Book.objects.get(name="Django")
+        web = Book.objects.get(name="Web")
+        self.assertEqual(djan.web, 'https://www.google.com')
+        self.assertEqual(web.price, 5.99)
